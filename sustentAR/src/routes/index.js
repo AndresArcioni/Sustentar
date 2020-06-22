f1bddacfc79ea45ba7e7f9aebf892347761bc2dc
Guidomp11 <ponceguido@gmail.com>
2020-06-22 13:44:02 -0300

    nuevas rutas

--- sustentAR/src/routes/index.js
+++ sustentAR/src/routes/index.js
@@ -6,7 +6,12 @@ const mainController = require(path.join(__dirname, '../controllers/mainControll
 
 //  <<--RUTAS-->>   //
 router.get('/', mainController.root); /* GET - home page */
+/*CARRITO*/
 router.get('/carrito', mainController.mostrarCarrito);
+router.get('/infoUsuarioCompra', mainController.mostrarCarrito);
+router.get('/modoDePago', mainController.mostrarCarrito);
+router.get('/finalizarCompra', mainController.mostrarCarrito);
+/*FIN CARRITO*/
 router.get('/busquedaAvanzada', mainController.busquedaAvanzada);
 router.get('/formularioProductos', mainController.formularioProductos)
 router.get('/cuenta', mainController.cuenta)
