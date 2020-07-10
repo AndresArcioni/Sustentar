390af0b3541e2fa8d9b30d0fcbf42618ceee0ac4
Guidomp11 <ponceguido@gmail.com>
2020-07-10 17:53:54 -0300

    crud usuario

--- sustentAR/src/controllers/userController.js
+++ sustentAR/src/controllers/userController.js
@@ -1,18 +1,71 @@
+const fs = require('fs');
+const path = require('path'); 
+let bcrypt = require('bcrypt');  
+
+let usuarios = fs.readFileSync(path.join(__dirname, '../data/usuarios.json'), 'utf8');
+usuarios = JSON.parse(usuarios);
+
 //  <<--USERCONTROLLER-->>   //
 module.exports = {
     cuenta: function(req, res){
-        res.render('cuenta')
+        res.render('cuenta', {usuario : usuarios[0]})//VER QUE PONER ACA
     },
     login: function(req, res){
         res.render('login')
     },
+    ingresarCuenta: function(req, res){
+        for(let i = 0; i < usuarios.length; i++){
+            if(usuarios[i].email == req.body.email && bcrypt.compareSync(req.body.contrasenia, usuarios[i].contrasenia)){
+                res.redirect('/')
+            }
+        }
+        res.render('login');
+    },
     registro: function(req, res){
         res.render('registro')
+    },
+    registrarNuevoUsuario : function(req, res){
+        let nuevoUsuario = {
+            id: Number(usuarios.length+1),
+            nombre: req.body.nombre,
+            apellido: req.body.apellido,
+            contrasenia: bcrypt.hashSync(req.body.contrasenia, 10),
+            email: req.body.email, 
+            dni: " ",
+            direccion: " ",
+            depto: " ",
+            codigoPostal: " ",
+            ciudad: " ",
+            entreCalles: " ",
+            nroTelefono: " "
+        }
+        usuarios.push(nuevoUsuario);
+        fs.writeFileSync(path.join(__dirname, '../data/usuarios.json'), JSON.stringify(usuarios));
+
+        res.redirect('/user/login');
     },
     misCompras: function(req, res){
         res.render('misCompras');
     }, 
     editarCuenta: function(req, res){
-        res.render('cuenta')
+        for(let i = 0; i < usuarios.length; i++){
+            if(usuarios[i].id == req.params.idUsuario){
+                usuarios[i].nombre =  req.body.nombre;
+                usuarios[i].apellido = req.body.apellido;
+                usuarios[i].email = req.body.email; 
+                usuarios[i].dni =  (req.body.dni == undefined) ? " " : req.body.dni;
+                usuarios[i].direccion = (req.body.direccion == undefined) ? " " : req.body.direccion;
+                usuarios[i].depto = (req.body.depto == undefined) ? " " :  req.body.depto;
+                usuarios[i].codigoPostal = (req.body.codigoPostal == undefined) ? " " : req.body.codigoPostal;
+                usuarios[i].ciudad =  (req.body.ciudad == undefined) ? " " : req.body.ciudad;
+                usuarios[i].entreCalles =  (req.body.entreCalles == undefined) ? " " : req.body.entreCalles;
+                usuarios[i].nroTelefono = (req.body.nroTelefono == undefined) ? " " : req.body.nroTelefono;
+
+                fs.writeFileSync(path.join(__dirname, '../data/usuarios.json'), JSON.stringify(usuarios));
+
+                res.render('cuenta', {usuario: usuarios[i]})
+            }
+        }
+        res.render('error');
     }
 };
--- sustentAR/src/routes/userRouter.js
+++ sustentAR/src/routes/userRouter.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const path = require('path');
 const userController = require(path.join(__dirname, '../controllers/userController.js'));
 
-router.get('/cuenta', userController.cuenta);
-router.post('/cuenta', userController.editarCuenta); //ES PUT EN REALIDAD
+router.get('/cuenta/:idUsuario', userController.cuenta);
+router.put('/cuenta/:idUsuario', userController.editarCuenta);
 router.get('/login', userController.login);
+router.post('/login', userController.ingresarCuenta);
 router.get('/registro', userController.registro);
+router.post('/registro', userController.registrarNuevoUsuario);
 router.get('/misCompras', userController.misCompras);
 
 module.exports = router;
