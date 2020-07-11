3ea5c8e7c65ad96cd96400113b5ed465b8afa40d
Guidomp11 <ponceguido@gmail.com>
2020-07-11 11:06:32 -0300

    home y listado de productos terminados

--- sustentAR/src/controllers/mainController.js
+++ sustentAR/src/controllers/mainController.js
@@ -1,6 +1,16 @@
+const fs = require('fs');
+const path = require('path');   
+
+let productos = fs.readFileSync(path.join(__dirname, '../data/productos.json'), 'utf8');
+productos = JSON.parse(productos);
+
 //  <<--MAINCONTROLLER-->>   //
 module.exports = {
     root: function(req, res) {
-        res.render('home')
-    }
+        let top4Productos = [];
+        for(let i = 0 ; i < 4; i++){
+            top4Productos.push(productos[i]);
+        }
+        res.render('home', {productos: top4Productos})
+    },
 };
