5718ad29f20c70d4c9e8943ffa13597aa9129525
Guidomp11 <ponceguido@gmail.com>
2020-08-25 09:25:21 -0300

    preview de imagen cambiada en proceso

---
+++ sustentAR/public/js/imgPreview.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function(){
+    let imgDeProducto = document.querySelectorAll('#imgDeProducto');//IMAGENES
+    let imgInputs = document.querySelectorAll('#imgDeProducto');//inputs de type file
+
+    
+})
